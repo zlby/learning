77a42e3412cf733e7c40aae522699dfed157a361
zlby <zlby970129@gmail.com>
2017-04-14 20:47:50 +0800

    Add files via upload

---
+++ tcp/tcp_client.py
@@ -0,0 +1,11 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+print(s.recv(1024).decode('utf-8'))
+
+for data in [b'Micheal', b'Tracy', b'Sarah']:
+	s.send(data)
+	print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
+s.close()
